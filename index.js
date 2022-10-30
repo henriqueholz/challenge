a2333c8cbb2cac885406f89c3fc1514d4a317a93
henriqueholz <henriqueholz@live.com>
2022-10-30 17:32:44 -0300

    Handling yes button and showing notification message

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ class ConfirmationDialog {
     var overlayme = document.getElementById('dialog-container')
     overlayme.style.display = 'block'
   }
+
+  hideDialog = () => {
+    document.getElementById('yes').onclick = function () {
+      confirm()
+    }
+    function confirm() {
+      var overlayme = document.getElementById('dialog-container')
+      overlayme.style.display = 'none'
+      showNotification()
+    }
+    function showNotification() {
+      let displayMessage = document.getElementById('notification')
+      console.log(displayMessage)
+      displayMessage.innerHTML = `You just clicked "yes".`
+    }
+  }
 }
 
 const confirmationLog = new ConfirmationDialog()
